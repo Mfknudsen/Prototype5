359e8cd96ba8370b639d653cc156c714233e9adf
andreeav19 <andra_368@yahoo.com>
2025-10-01 13:42:48 +0200

    added random spawn method for npc model

--- Prototype5/Assets/Scripts/NPCs/NpcBehaviour.cs
+++ Prototype5/Assets/Scripts/NPCs/NpcBehaviour.cs
@@ -1,23 +1,26 @@
+using Unity.Mathematics;
 using UnityEngine;
 using UnityEngine.AI;
+using Random = UnityEngine.Random;
 
 namespace NPCs
 {
-    public class NpcMovement : MonoBehaviour
+    public class NpcBehaviour : MonoBehaviour
     {
         [SerializeField] private float maxDistanceToTarget = 20.0f;
         [SerializeField] private float minDistanceToTarget = 4.0f;
         [SerializeField] private float npcRadius = 15.0f;
-        [SerializeField] private float speed = 2.5f;
         [SerializeField] private Transform targetTransform;
-    
+        [SerializeField] private GameObject[] npcPrefabs;
+        [SerializeField] private Vector3 spawnPrefabOffset = new Vector3(0.0f, 0.8f, 0.0f);
+        
         private NavMeshAgent _agent;
         private bool _hasArrived = false;
     
         void Start()
         {
+            SpawnRandomPrefab();
             _agent = GetComponent<NavMeshAgent>();
-            _agent.speed = speed;
         }
 
         void Update()
@@ -60,6 +63,20 @@ namespace NPCs
                 NavMeshHit hit;
                 if (NavMesh.SamplePosition(randomSpherePoint, out hit, npcRadius, NavMesh.AllAreas))
                     _agent.SetDestination(hit.position);   
+            }
+        }
+
+        void SpawnRandomPrefab()
+        {
+            if (npcPrefabs.Length >= 1)
+            {
+                int random = Random.Range(0, npcPrefabs.Length);
+                Instantiate(npcPrefabs[random], transform.position + spawnPrefabOffset,
+                    Quaternion.identity, transform);                
+            }
+            else
+            {
+                Debug.Log("NPC prefab list is empty!");
             }
         }
     }
